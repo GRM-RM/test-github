45b4ef46b0b16e8a3698bda37a7686a1d84b783b
GRM-RM <32810772+GRM-RM@users.noreply.github.com>
2017-11-15 17:07:40 +0800

    加注释

--- src/main/java/grm/GuoTest.java
+++ src/main/java/grm/GuoTest.java
@@ -7,6 +7,9 @@ package grm;
  */
 public class GuoTest {
     public static void main(String[] args) {
+        /**
+         * 一只特立独行的猪
+         */
         System.out.println("郭如梦的测试");
     }
 }
