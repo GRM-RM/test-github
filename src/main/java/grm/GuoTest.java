0e6d42a6d4f8611e96884d78c7e57f92a3347768
lvzizheng <32810772+GRM-RM@users.noreply.github.com>
2017-11-15 16:19:06 +0800

    Initial commit

---
+++ src/main/java/grm/GuoTest.java
@@ -0,0 +1,12 @@
+package grm;
+
+/**
+ * A unique of the lion
+ *
+ * @author grm
+ */
+public class GuoTest {
+    public static void main(String[] args) {
+        System.out.println("郭如梦的测试");
+    }
+}
